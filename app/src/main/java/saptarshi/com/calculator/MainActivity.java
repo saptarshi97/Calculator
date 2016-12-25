e0eba60aed30f930326e7ea3152bebc7d78ec66a
Saptarshi Roy Choudhuri <saptarshi.roychoudhuri@gmail.com>
2016-12-25 12:52:47 +0530

    Modified files committed

--- app/src/main/java/saptarshi/com/calculator/MainActivity.java
+++ app/src/main/java/saptarshi/com/calculator/MainActivity.java
@@ -10,44 +10,56 @@ public class MainActivity extends AppCompatActivity {
     EditText number1,number2;
     TextView result,Display;
     double num1,num2,sum,difference,product,quotient,remainder,power;
+    String t1,t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        number1 = (EditText)findViewById(R.id.input_2);
-        number2 = (EditText)findViewById(R.id.input_1);
-        result = (TextView)findViewById(R.id.Operation_Result);
-        Display = (TextView)findViewById(R.id.display);
+        number1 = (EditText) findViewById(R.id.input_2);
+        number2 = (EditText) findViewById(R.id.input_1);
+        result = (TextView) findViewById(R.id.Operation_Result);
+        Display = (TextView) findViewById(R.id.display);
+        t1 = number1.getText().toString();
+        t2 = number2.getText().toString();
+        if(t1.equals("") || t2.equals(""))
+        {
+            if(t1.equals(""))
+            num1=0;
+            else if(t2.equals(""))
+                num2=0;
+            else
+            {
+                num1=0;
+                num2=0;
+            }
+        }
+        else
+        {
+            num1=Double.parseDouble(t1);
+            num2=Double.parseDouble(t2);
+        }
     }
     public void sendAddition(View view)
     {
-        num1 = Double.parseDouble(number1.getText().toString());
-        num2 = Double.parseDouble(number2.getText().toString());
         Display.setText(Double.toString(num1)+" + "+Double.toString(num2));
         sum = num1 + num2;
         result.setText(Double.toString(sum));
     }
     public void sendSubtraction(View view)
     {
-        num1 = Double.parseDouble(number1.getText().toString());
-        num2 = Double.parseDouble(number2.getText().toString());
         Display.setText(Double.toString(num1)+" - "+Double.toString(num2));
         difference = num1 - num2;
         result.setText(Double.toString(difference));
     }
     public void sendProduct(View view)
     {
-        num1 = Double.parseDouble(number1.getText().toString());
-        num2 = Double.parseDouble(number2.getText().toString());
         Display.setText(Double.toString(num1)+" * "+Double.toString(num2));
         product= num1 * num2;
         result.setText(Double.toString(product));
     }
     public void sendQuotient(View view)
     {
-        num1 = Double.parseDouble(number1.getText().toString());
-        num2 = Double.parseDouble(number2.getText().toString());
         Display.setText(Double.toString(num1)+" / "+Double.toString(num2));
         if(num2==0)
             result.setText("Error ! Division by zero !");
@@ -57,8 +69,6 @@ public class MainActivity extends AppCompatActivity {
     }
     public void sendRemainder(View view)
     {
-        num1 = Double.parseDouble(number1.getText().toString());
-        num2 = Double.parseDouble(number2.getText().toString());
         Display.setText(Double.toString(num1)+" % "+Double.toString(num2));
         if(num2==0)
             result.setText("Error ! Division by zero !");
@@ -68,8 +78,6 @@ public class MainActivity extends AppCompatActivity {
     }
     public void sendPower(View view)
     {
-        num1 = Double.parseDouble(number1.getText().toString());
-        num2 = Double.parseDouble(number2.getText().toString());
         Display.setText(Double.toString(num1)+" ^ "+Double.toString(num2));
         power = Math.pow(num1,num2);
         result.setText(Double.toString(power));
